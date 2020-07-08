170d7e7e792f8b80952a70c7bfb395ea4957117a
Carlos Graybill <carlos.a.graybill@gmail.com>
2020-07-08 18:53:01 -0400

    created the .click event and referenced Jquery

--- JS/main.js
+++ JS/main.js
@@ -1,17 +1,6 @@
-<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"> </script>
-
-$(function () {
-    $('search').click(function () {
-      
-    })   
+$( document ).ready(function() {
+    console.log( "ready!" );
+    $( "#search" ).click(function() {
+        alert( "Handler for .click() called." );
+      });
 });
-
-$ (document) .ready(function() {
-
-    var urlGifs = 'api.giphy.com/v1/gifs/search'
-    var urlStickers = 'api.giphy.com/v1/stickers/search'
-    var key = 'qCTweoTLPCLppqQ6z3Wz4z2DBnV0Tfzj'
-});
-
-var xhr = $.get("http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=qCTweoTLPCLppqQ6z3Wz4z2DBnV0Tfzj&limit=5");
-xhr.done(function (data) { console.log("success got data", data); });
