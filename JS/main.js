712c190f794fb6103e9ad3cc57f61b78d39f199a
Carlos Graybill <carlos.a.graybill@gmail.com>
2020-07-08 22:04:15 -0400

    allowing for user specifications

--- JS/main.js
+++ JS/main.js
@@ -1,7 +1,16 @@
 $(function () {
     console.log('Ready!')
+    var apikey = 'qCTweoTLPCLppqQ6z3Wz4z2DBnV0Tfzj';
+    var q = $('#q');
+    var limit = $('#limit');
+    var rating = $('#rating');
+
     $( "#search" ).click(function () {
         console.log('Clicked!')
         $("#output").text("Searching...") 
+        $.getJSON('https://api.giphy.com/v1/gifs/search?api_key=qCTweoTLPCLppqQ6z3Wz4z2DBnV0Tfzj&q=nuke&limit=25&offset=0&rating=g&lang=en', function(data){
+            console.log(data)
+        })
+
     })
 });
