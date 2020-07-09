5aa391aa549134552d1f152409f7f3ed84ac65dc
Carlos Graybill <carlos.a.graybill@gmail.com>
2020-07-08 22:18:39 -0400

    allowing for user inputs to define URL

--- JS/main.js
+++ JS/main.js
@@ -1,14 +1,13 @@
 $(function () {
     console.log('Ready!')
-    var apikey = 'qCTweoTLPCLppqQ6z3Wz4z2DBnV0Tfzj';
-    var q = $('#q');
-    var limit = $('#limit');
-    var rating = $('#rating');
-
     $( "#search" ).click(function () {
-        console.log('Clicked!')
+        var apikey = 'qCTweoTLPCLppqQ6z3Wz4z2DBnV0Tfzj';
+        var q = $('#q').val();
+        var limit = $('#limit').val();
+        var rating = $('#rating').val();
+        console.log('Clicked!',apikey, q, limit, rating)
         $("#output").text("Searching...") 
-        $.getJSON('https://api.giphy.com/v1/gifs/search?api_key=qCTweoTLPCLppqQ6z3Wz4z2DBnV0Tfzj&q=nuke&limit=25&offset=0&rating=g&lang=en', function(data){
+        $.getJSON('https://api.giphy.com/v1/gifs/search?api_key=' + apikey + '&q=' + q+ '&limit=' + limit +' &offset=0&rating=' + rating + '&lang=en', function(data){
             console.log(data)
         })
 
